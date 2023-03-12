99614974bde4fa6912ea52e9d08bccea51ab0159
annaroskach <danuta_tyt@mail.ru>
2023-03-12 17:54:21 +0300

    homework15

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle("Велосипед 1");
+        Car car = new Car("Легковое авто", 4);
+        Truck truck = new Truck("Грузовое авто", 4);
+
+
+        ServiceStation serviceStation = new ServiceStation();
+
+        System.out.println("Диагностика велосипеда: ");
+        serviceStation.check(vehicle);
+        System.out.println("________________________");
+
+        System.out.println("Диагностика легкового авто: ");
+        serviceStation.check(car);
+        System.out.println("________________________");
+
+        System.out.println("Диагностика грузового авто: ");
+        serviceStation.check(truck);
+        System.out.println("________________________");
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,6 @@
+public class Vehicle extends Transport {
+    private static final int WHEELS_COUNT = 2;
+    public Vehicle(String modelName) {
+        super(modelName, WHEELS_COUNT);
+    }
+}
